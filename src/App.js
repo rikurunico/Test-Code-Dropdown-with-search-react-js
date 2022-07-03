031fc6df2e2d32baebbfb7cec5b4fbd76bfd928c
nicoco1173 <rikurunico@gmail.com>
2022-07-03 12:31:35 +0700

    Coba API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Select from "react-select";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [select, setSelected] = useState([]);
+  const [searchData, setSearchData] = useState("");
+  const [menampilkan, setMenampilkan] = useState(false);
+
+  const getBerry = async () => {
+    const berryapi = await fetch("https://pokeapi.co/api/v2/berry/");
+    const value = await berryapi.json();
+    let result = value.results.map((data) => {
+      return {
+        value: data.name,
+        label: data.name,
+      };
+    });
+    setSelected(result.sort((a, b) => a.label.localeCompare(b.label)));
+  };
+
+  useEffect(() => {
+    getBerry();
+  }, []);
+
+  const handleSubmit = () => {
+    setMenampilkan((state) => !state);
+  };
+
+  const handleChange = (value) => {
+    setSearchData(value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{menampilkan ? searchData : ""}</h1>
+      <button onClick={() => handleSubmit()} disabled={!searchData}>
+        {menampilkan ? "Hide Button" : "Show Values"}
+      </button>
+      <br />
+      <br />
+      <Select options={select} onChange={(e) => handleChange(e.value)}></Select>
     </div>
   );
 }
